6ecb2e2a931d88c053eee68f755fb09e25d9c7b9
afrobug <afrojahmed6@gmail.com>
2023-05-06 00:26:56 +0530

    change

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,21 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
 
-class App extends Component {
-    render() {
+class DiwaliShoppingList extends Component {
+  render() {
+    const relatives = ["Amit", "Rita", "Rahul", "Kavya"];
 
-        return(
-            <div id="main">
-               {/* Do not remove the main div */}
-            </div>
-        )
-    }
+    return (
+      <div>
+        <h2>Relatives to Visit</h2>
+        <ol key="relativeList">
+          {relatives.map((relative, index) => (
+            <li key={`relativeListItem${index + 1}`}>{relative}</li>
+          ))}
+        </ol>
+      </div>
+    );
+  }
 }
 
-
-export default App;
+export default DiwaliShoppingList;
